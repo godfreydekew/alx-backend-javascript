d69c26a65df71cc6b2fef621fd8adfbd5d985acf
godfreydekew <119779590+godfreydekew@users.noreply.github.com>
2024-06-26 15:08:31 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup() {
   const create = createUser();
   return Promise
     .all([upload, create])
-    .then(result => (console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`)))
+    .then(result => {console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);})
     .catch(error => (error.log('Signup system offline')));
 }
