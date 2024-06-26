f489a4e8b2a1a11759280922641e22c852a7128b
godfreydekew <dekewgodfrey@gmail.com>
2024-06-26 17:59:15 +0000

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => {
-      const res = [];
-      result.forEach((element) => {
-        res.push({
-          status: element.status,
-          body: element.status === 'fulfilled' ? element.value : element.reason,
-        });
-        return res;
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    .then((res) => (
+      res.map((x) => ({
+        status: x.status,
+        value: x.status === 'fulfilled' ? x.value : String(x.reason),
+      }))
+    ));
 }
