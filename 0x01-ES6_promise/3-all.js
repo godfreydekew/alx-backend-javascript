6a77ac09b4c8472e224f7d8883c337627ed7c3d2
godfreydekew <dekewgodfrey@gmail.com>
2024-06-26 12:23:50 +0000

    Fix eslint erros

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then(result => {
+    .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
-    .catch(error => (error.log('Signup system offline')));
+    .catch((error) => (error.log('Signup system offline')));
 }
