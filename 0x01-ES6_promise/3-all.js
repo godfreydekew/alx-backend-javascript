99602ce7d6ab5637bc8fb865fffef3f47a13f316
godfreydekew <119779590+godfreydekew@users.noreply.github.com>
2024-06-26 15:15:56 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then(result => {
