54602a9d0f7424478b5808ec4c4d18bf4ed82fc7
godfreydekew <dekewgodfrey@gmail.com>
2024-06-29 07:32:16 +0000

    Task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+Import Building from './5-building'
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+  set floors(value) {
+    this._floors = value;
+  }
+  get floors() {
+    return this._floors;
+  }
+  evacuationWarningMessage() {
+    return "Evacuate slowly the NUMBER_OF_FLOORS floors";
+  }
+}
