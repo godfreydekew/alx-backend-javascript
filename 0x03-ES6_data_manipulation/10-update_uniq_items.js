34c75e0d3dd5e716862e63aeee00a115163677a5
godfreydekew <dekewgodfrey@gmail.com>
2024-07-03 06:21:01 +0000

    Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,6 +2,9 @@ export default function updateUniqueItems(myMap) {
   for (const k of myMap.keys()) {
     if (myMap.get(k) === 1) {
       myMap.set(k, 100);
+      if (myMap.get(k) === 1) {
+        throw new Error('Cannot process');
+      }
     }
   }
 }
