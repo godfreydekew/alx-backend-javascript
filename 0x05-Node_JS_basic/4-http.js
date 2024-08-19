9427e8bf649322d30bc4548f72f33771fc835d20
godfreydekew <dekewgodfrey@gmail.com>
2024-08-19 14:38:48 +0300

    Update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-server.listen(1245, () => {});
+app.listen(1245, () => {});
 
-module.exports = server;
+module.exports = app;
