6807986c6ce7a6790e80b57d26a418a3a24b587a
godfreydekew <dekewgodfrey@gmail.com>
2024-08-19 21:22:28 +0300

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-var stdin = process.openStdin();
+const stdin = process.openStdin();
 
 stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?');
-stdin.on('data', function (chunk) {
-  process.stdout.write('Your name is: ' + chunk);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+stdin.on('data', (chunk) => {
+  process.stdout.write(`Your name is: ${chunk}`);
   stdin.pause();
 });
 
-stdin.on('end', function () {
+stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
